a5ad0396825944417b19087ae1561b6e655a76c9
Du <du-nguyen@ecombest.com>
2022-06-15 17:36:20 +0700

    init source

---
+++ assets/est-shipping.js
@@ -0,0 +1,9 @@
+class ESTShipping extends HTMLElement {
+    constructor () {
+        super();
+        const wrapper = document.createElement('div');
+        console.log("OK")
+    }
+}
+
+customElements.define('est-shipping', ESTShipping);
